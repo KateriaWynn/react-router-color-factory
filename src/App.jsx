e6a76a2dfbce85774ce8e2d7482f93aa9c32a424
Kateria Wynn <kwynn1211@gmail.com>
2021-07-09 16:13:01 -0400

    updated Routes within Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import Routes from './routes/Routes';
 import './app.scss';
 
-function App() {
+export default function App({ colors }) {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <div className="wrapper">
+        <div className="left">
+          <p className="logo">The Color Factory.</p>
+        </div>
+      </div>
+      <div className="colors">
+        <Router>
+          <Routes colors={colors} />
+        </Router>
+      </div>
     </div>
   );
 }
 
-export default App;
+App.defaultProps = {
+  colors: ['red', 'green', 'yellow', 'orange', 'purple'],
+};
